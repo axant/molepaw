4cd0afa9d19d9f5373214e394a23afb38f5e5cc9
sergio brero <sergio.brero.it@gmail.com>
2020-01-22 15:00:26 +0100

    8.5 - Ridisegno datasource/dataset per mongoDB con corner case

--- etl/lib/dbsessions/mongodb.py
+++ etl/lib/dbsessions/mongodb.py
@@ -2,6 +2,8 @@
 import json
 from pandas import DataFrame
 import re
+from tg.i18n import ugettext as _
+from tg.exceptions import HTTPPreconditionFailed
 
 
 class MongoDBSession(object):
@@ -12,14 +14,24 @@ class MongoDBSession(object):
         self._collection = None
 
     def set_collection(self, collection):
-        self._collection = self._session.get_default_database()\
-            .get_collection(collection)
+        if collection in self._session.get_default_database().list_collection_names():
+            self._collection = self._session.get_default_database() \
+                .get_collection(collection)
+        else:
+            raise HTTPPreconditionFailed(
+                detail='The requested collection: %s doesn\'t exist, check it' % collection
+            )
 
     def extract_directive_value(self, query, directive_pattern):
         pattern = r'^#' + re.escape(directive_pattern) + r'.*'
         directive_match = re.search(pattern, query)
-        value = directive_match.group(0).split('=')[-1].strip()
-        return value, directive_match
+        if directive_match:
+            value = directive_match.group(0).split('=')[-1].strip()
+            return value, directive_match
+        else:
+            raise HTTPPreconditionFailed(
+                detail=_('Wrong collection directive syntax, it should be something like "#collection=collectionname"')
+            )
 
     def parse_q(self, q):
         collection_name, sub_re = self.extract_directive_value(q, 'collection')
@@ -27,7 +39,10 @@ class MongoDBSession(object):
         return re.sub(sub_re.group(0), '', q)
 
     def execute(self, q):
-        query = json.loads(self.parse_q(q))
+        try:
+            query = json.loads(self.parse_q(q))
+        except (TypeError, ValueError) as ex:
+            raise HTTPPreconditionFailed(_('Wrong query format, query must be valid json'))
         if isinstance(query, dict):
             data = self._collection.find(query)
         elif isinstance(query, list):
