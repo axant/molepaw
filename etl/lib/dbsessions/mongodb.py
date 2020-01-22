2212ab54f0d9b75205841a5ef09b42aafb95a535
sergio brero <sergio.brero.it@gmail.com>
2020-01-22 14:10:17 +0100

    8.5 Ridisegno datasource per mongodb

--- etl/lib/dbsessions/mongodb.py
+++ etl/lib/dbsessions/mongodb.py
@@ -1,27 +1,37 @@
 # -*- coding: utf-8 -*-
 import json
 from pandas import DataFrame
+import re
 
 
 class MongoDBSession(object):
+
     def __init__(self, url):
         from pymongo import MongoClient
-
-        try:
-            url, collection = url.rsplit('#', 1)
-        except ValueError:
-            raise ValueError('url does not provide the collection name, splace specify url in form '
-                             'mongodb://user:pass@host:port/database#collection')
-
         self._session = MongoClient(url)
-        self._collection = self._session.get_default_database()[collection]
+        self._collection = None
+
+    def set_collection(self, collection):
+        self._collection = self._session.get_default_database()\
+            .get_collection(collection)
+
+    def extract_directive_value(self, query, directive_pattern):
+        pattern = r'^#' + re.escape(directive_pattern) + r'.*'
+        directive_match = re.search(pattern, query)
+        value = directive_match.group(0).split('=')[-1].strip()
+        return value, directive_match
+
+    def parse_q(self, q):
+        collection_name, sub_re = self.extract_directive_value(q, 'collection')
+        self.set_collection(collection_name)
+        return re.sub(sub_re.group(0), '', q)
 
     def execute(self, q):
-        q = json.loads(q)
-        if isinstance(q, dict):
-           data = self._collection.find(q)
-        elif isinstance(q, list):
-           data = self._collection.aggregate(q)
+        query = json.loads(self.parse_q(q))
+        if isinstance(query, dict):
+            data = self._collection.find(query)
+        elif isinstance(query, list):
+            data = self._collection.aggregate(query)
         return DataFrame(list(data))
 
     def rollback(self):
