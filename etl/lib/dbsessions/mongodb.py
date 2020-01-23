31a1df01eca371aa4a7cd1c6c82d8018a51193c3
sergio brero <sergio.brero.it@gmail.com>
2020-01-23 14:09:49 +0100

    8.5 - Collection valid both in url and in directive

--- etl/lib/dbsessions/mongodb.py
+++ etl/lib/dbsessions/mongodb.py
@@ -7,11 +7,20 @@ from tg.exceptions import HTTPPreconditionFailed
 
 
 class MongoDBSession(object):
+    _session = None
+    _collection = None
 
     def __init__(self, url):
+        collection = None
+        if u'#' in url:
+            url, collection = url.rsplit('#', 1)
+        self.set_session(url)
+        if collection:
+            self.set_collection(collection)
+
+    def set_session(self, url):
         from pymongo import MongoClient
         self._session = MongoClient(url)
-        self._collection = None
 
     def set_collection(self, collection):
         if collection in self._session.get_default_database().list_collection_names():
@@ -28,27 +37,35 @@ class MongoDBSession(object):
         if directive_match:
             value = directive_match.group(0).split('=')[-1].strip()
             return value, directive_match
+        elif not directive_match and self._collection is not None:
+            return None, None
         else:
             raise HTTPPreconditionFailed(
-                detail=_('Wrong collection directive syntax, it should be something like "#collection=collectionname"')
+                detail=_(
+                    'Missing collection, it should be something like "#collection=collectionname" in the query or'
+                    'specify the url in form mongodb://user:pass@host:port/database#collection'
+                )
             )
 
     def parse_q(self, q):
         collection_name, sub_re = self.extract_directive_value(q, 'collection')
-        if collection_name not in self._session.get_default_database().list_collection_names():
-            raise HTTPPreconditionFailed(
-                detail='The requested collection: %s doesn\'t exist, check it' % collection_name
-            )
-        query = re.sub(sub_re.group(0), '', q)
+        if collection_name is not None and sub_re is not None:
+            if collection_name not in self._session.get_default_database().list_collection_names():
+                raise HTTPPreconditionFailed(
+                    detail='The requested collection: %s doesn\'t exist, check it' % collection_name
+                )
+            query = re.sub(sub_re.group(0), '', q)
+            self.set_collection(collection_name)
+        else:
+            query = q
         try:
             query = json.loads(query)
         except (TypeError, ValueError) as ex:
             raise HTTPPreconditionFailed(_('Wrong query format, query must be valid json'))
-        return query, collection_name
+        return query
 
     def execute(self, q):
-        query, collection = self.parse_q(q)
-        self.set_collection(collection)
+        query = self.parse_q(q)
         if isinstance(query, dict):
             data = self._collection.find(query)
         elif isinstance(query, list):
